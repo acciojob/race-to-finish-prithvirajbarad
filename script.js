ac2d091a52506ec0a422f6b9eb1212c7f8885ae9
prithvirajbarad <112259618+prithvirajbarad@users.noreply.github.com>
2023-10-22 19:35:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,24 @@
 window.promises = [];
-
-// Do not change the code above this
-// add your promises to the array `promises`
-
-let promise1 = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve("Promise1");
-    }, Math.floor(Math.random() * 5000) + 1000);
-});
-
-let promise2 = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve("Promise2");
-    }, Math.floor(Math.random() * 5000) + 1000);
-});
-
-let promise3 = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve("Promise3");
-    }, Math.floor(Math.random() * 5000) + 1000);
-});
-
-let promise4 = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve("Promise4");
-    }, Math.floor(Math.random() * 5000) + 1000);
-});
-
-let promise5 = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve("Promise5");
-    }, Math.floor(Math.random() * 5000) + 1000);
-});
-
-const myArray = [promise1, promise2, promise3, promise4, promise5];
-
-Promise.any(myArray).then((result) => {
-    document.getElementById("output").innerText = result;
-});
+ 
+// Create an array of five promises with random delays between 1 and 5 seconds
+for (let i = 0; i < 5; i++) {
+  const delay = Math.floor(Math.random() * 5) + 1;
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Promise ${i + 1} resolved after ${delay} seconds`);
+    }, delay * 1000);
+  });
+  promises.push(promise);
+}
+ 
+// Wait for the first promise to resolve using Promise.any()
+Promise.any(promises)
+  .then((result) => {
+    // Print the result to the output div
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = result;
+  })
+  .catch((error) => {
+    // Handle errors if no promise resolves
+    console.log('All promises rejected:', error);
+  });
